3152548572ba69860b688590fa6c859e0f5c37d7
Jisoo Park <jso9603@gmail.com>
2020-04-18 15:39:06 +0900

    Update indeed.py

--- requesting-each-pages/indeed.py
+++ requesting-each-pages/indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
   result = requests.get(URL)
 
   soup = BeautifulSoup(result.text, "html.parser")
@@ -41,7 +41,7 @@ def extract_job(html):
     'link': f"https://www.indeed.com/viewjob?jk={job_id}"
   }
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
     print(f"Scrapping page: {page}")
@@ -51,4 +51,9 @@ def extract_indeed_jobs(last_page):
     for result in results:
       job = extract_job(result)
       jobs.append(job)
+  return jobs
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
   return jobs
