051c86bf9581f90050c8a42209ab401ca02a247a
Jisoo Park <jso9603@gmail.com>
2020-05-03 14:06:43 +0900

    Update so.py

--- requesting-each-pages/so.py
+++ requesting-each-pages/so.py
@@ -10,6 +10,15 @@ def get_last_page():
   last_page = pages[-2].get_text(strip=True)
   return int(last_page)
 
+def extract_job(html):
+  title = html.find("div",{"class":"grid--cell fl1"}).find("h2").find("a")["title"]
+  company, location = html.find("div", {"class": "grid--cell fl1"}).find("h3").find_all("span", recursive=False)
+  company = company.get_text(strip=True)
+  location = location.get_text(strip=True).strip("-").strip("\r").strip("\n")
+  print(company, location)
+  return {'title': title}
+
+
 def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
@@ -17,8 +26,9 @@ def extract_jobs(last_page):
     soup = BeautifulSoup(result.text, "html.parser")
     results = soup.find_all("div", {"class", "-job"})
     for result in results:
-      print(result["data-jobid"])
-
+      job = extract_job(result)
+      jobs.append(job)
+  return jobs
 
 def get_jobs():
   last_page = get_last_page()
